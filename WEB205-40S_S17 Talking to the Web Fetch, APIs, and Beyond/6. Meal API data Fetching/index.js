70fc80f98082af51c2623fc89cb7479e8290cdc7
Tushar Wasake <tusharwasake@gmail.com>
2024-11-15 04:32:36 +0530

    WEB17

--- WEB205-40S_S17 Talking to the Web Fetch, APIs, and Beyond/6. Meal API data Fetching/index.js
+++ WEB205-40S_S17 Talking to the Web Fetch, APIs, and Beyond/6. Meal API data Fetching/index.js
@@ -1,14 +1,44 @@
 // fetch("https://www.themealdb.com/api.php?ref=apilist.fun")
 
 
-fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-.then(function(response){
-    // console.log(response.json())
-    return response.json()
-})
-.then(data => {
-    console.log(data); 
-    return data;// Now logs the actual data, not a pending Promise
-  })
+function getCategoryData(){
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then(function(response){
+        // console.log(response.json())
+        return response.json()
+    })
+    .then(data => {
+        // console.log(data); 
+        data.meals.forEach(element => {
+            console.log(element)
+        });
+     
+    })
+    .catch(function(Error){
+        return Error;   
+    })
+
+};
 
 
+function getIngredientData(){
+    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+    .then(response=>{
+        console.log(response)
+        return response.json()
+    })
+    .then(IngredientData=>{
+        console.log(IngredientData)
+        IngredientData.meals.forEach(function(Ingredient){
+            console.log(Ingredient)
+        })
+    })
+    
+}
+
+
+let getCategoryDataButton = document.getElementById("get-category-data");
+let getIngredientDataButton = document.getElementById("getIngredientData");
+
+getCategoryDataButton.addEventListener("click",getCategoryData)
+getIngredientDataButton.addEventListener("click",getIngredientData)
--- WEB205-40S_S17 Talking to the Web Fetch, APIs, and Beyond/7. Fetching and DIsplaying User from Requres Api/index.js
+++ WEB205-40S_S17 Talking to the Web Fetch, APIs, and Beyond/7. Fetching and DIsplaying User from Requres Api/index.js
@@ -1,1 +1,41 @@
-console.log(fetch("https://www.themealdb.com/api.php?ref=apilist.fun"))
+
+function userData(){
+    fetch("https://dummyjson.com/users")
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+
+        allUser(data.users)
+    })
+    .catch(function(error){
+        console.error("something went wrong");
+        
+    })
+};
+
+
+function allUser(people){
+    const users = document.getElementById("container");
+    users.innerHTML = ""; 
+    people.forEach(element => {
+        const userElement = document.createElement("div");
+        
+        userElement.className = "users";
+
+        userElement.innerHTML=
+        `
+            <h1>Name: ${element.firstName}, middle:${element.maidenName}, Last: ${element.lastName}</h1>
+            <p>${element.email}</p>
+        `
+        users.appendChild(userElement)
+
+        
+    });
+    
+}
+
+
+
+
+
